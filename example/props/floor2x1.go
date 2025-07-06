5527f2a84cf71661b660ba7283c85084029aaf82
Björn Ritzl <bjorn.ritzl@gmail.com>
2025-07-07 00:28:38 +0200

    movement, physics, event handling

--- example/props/floor2x1.go
+++ example/props/floor2x1.go
@@ -12,3 +12,30 @@ components {
     x: 2.0
   }
 }
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_STATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"floor\"\n"
+  "mask: \"player\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      y: -0.5\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "    id: \"floor\"\n"
+  "  }\n"
+  "  data: 4.0\n"
+  "  data: 0.5\n"
+  "  data: 2.0\n"
+  "}\n"
+  ""
+}
